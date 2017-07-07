43a8a54a02442cf192bfb52a7accbe7400dee5dc
tiedie211 <tchristiand@gmail.com>
2017-07-06 21:25:29 -0700

    Xcode error fixes

--- Cru/NotificationsViewController.swift
+++ Cru/NotificationsViewController.swift
@@ -60,21 +60,6 @@ class NotificationsViewController: UITableViewController, DZNEmptyDataSetDelegat
         self.tableView.separatorColor = UIColor.clear
         let attributes = [ NSFontAttributeName: UIFont(name: Config.fontName, size: 18)!, NSForegroundColorAttributeName: UIColor.black]
         return NSAttributedString(string: "You do not have any notifications!", attributes: attributes)
-        
-    }
-
-    //Set the text to be displayed when the table is empty
-    func title(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
-        self.tableView.separatorColor = UIColor.clear
-        let attributes = [ NSFontAttributeName: UIFont(name: Config.fontName, size: 18)!, NSForegroundColorAttributeName: UIColor.black]
-        return NSAttributedString(string: "You do not have any notifications!", attributes: attributes)
-    }
-
-    //Set the text to be displayed when the table is empty
-    func title(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
-        self.tableView.separatorColor = UIColor.clear
-        let attributes = [ NSFontAttributeName: UIFont(name: Config.fontName, size: 18)!, NSForegroundColorAttributeName: UIColor.black]
-        return NSAttributedString(string: "You do not have any notifications!", attributes: attributes)
     }
 
     override func didReceiveMemoryWarning() {
