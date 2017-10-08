846810d658879e7763c5eefad4d4e8f5ed5a3010
tiedie211 <tchristiand@gmail.com>
2017-10-07 17:10:31 -0700

    Fixed issue #143 - Summer Missions: Details not loading

--- Cru/SummerMissionsTableViewCell.swift
+++ Cru/SummerMissionsTableViewCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class SummerMissionsTableViewCell: UITableViewCell {
+    
     @IBOutlet weak var missionImage: UIImageView!
     @IBOutlet weak var missionName: UILabel!
     @IBOutlet weak var missionDateLabel: UILabel!
@@ -18,22 +19,34 @@ class SummerMissionsTableViewCell: UITableViewCell {
     var mission: SummerMission! {
         didSet {
             let dateFormat = "MMM d, yyyy"
-            let startDate = GlobalUtils.stringFromDate(mission.startNSDate, format: dateFormat)
-            let endDate = GlobalUtils.stringFromDate(mission.endNSDate, format: dateFormat)
+            let startDate = GlobalUtils.stringFromDate(self.mission.startNSDate, format: dateFormat)
+            let endDate = GlobalUtils.stringFromDate(self.mission.endNSDate, format: dateFormat)
             let date = startDate + " - " + endDate
             
-            if mission.imageLink != "" {
-                let urlRequest = URLRequest(url: URL(string: mission.imageLink)!)
+            if self.mission.imageLink != "" {
+                let urlRequest = URLRequest(url: URL(string: self.mission.imageLink)!)
                 CruClients.getImageUtils().getImageDownloader().download(urlRequest) { response in
                     if let image = response.result.value {
                         self.missionImage.image = image
                     }
                 }
+            } else {
+                self.missionImage.image = nil
             }
-            //missionImage.load.request(with: mission.imageLink)
-            missionName.text = mission.name
-            missionDateLabel.text = date
-            missionLocation.text = mission.getLocationString()
+            self.missionName.text = self.mission.name
+            self.missionDateLabel.text = date
+            self.missionLocation.text = self.mission.getLocationString()
         }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.selectionStyle = UITableViewCellSelectionStyle.none
+        
+        self.card.layer.shadowColor = UIColor.black.cgColor
+        self.card.layer.shadowOffset = CGSize(width: 0, height: 1)
+        self.card.layer.shadowOpacity = 0.25
+        self.card.layer.shadowRadius = 2
     }
 }
--- Cru/SummerMissionsTableViewController.swift
+++ Cru/SummerMissionsTableViewController.swift
@@ -10,53 +10,46 @@ import UIKit
 import DZNEmptyDataSet
 import MRProgress
 
-class SummerMissionsTableViewController: UITableViewController, SWRevealViewControllerDelegate, DZNEmptyDataSetDelegate,DZNEmptyDataSetSource {
+class SummerMissionsTableViewController: UITableViewController {
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
-    var hasConnection = true
     
-    var missions = [SummerMission]()
+    fileprivate var missions = [SummerMission]()
+    fileprivate var hasConnection = true
     fileprivate let reuseIdentifier = "missionCell"
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Setup side menu
         GlobalUtils.setupViewForSideMenu(self, menuButton: menuButton)
         
+        // Configure nav bar
+        self.navigationItem.title = "Summer Missions"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont(name: Config.fontBold, size: 20)!, NSForegroundColorAttributeName: UIColor.white]
+        
+        // Start animating the loading overlay
         MRProgressOverlayView.showOverlayAdded(to: self.view, animated: true)
-        //Check for connection then load events in the completion function
+        
+        // Check for connection then download data
         CruClients.getServerClient().checkConnection(self.finishConnectionCheck)
         
-        
-        navigationItem.title = "Summer Missions"
-        
-        self.navigationController!.navigationBar.titleTextAttributes  = [ NSFontAttributeName: UIFont(name: Config.fontBold, size: 20)!, NSForegroundColorAttributeName: UIColor.white]
-        
-        //log Firebase Analytics Event
+        // Log Firebase Analytics Event
         Analytics.logEvent("Summer_missions_loaded", parameters: nil)
     }
     
     // MARK: - Helper Functions
-    //Test to make sure there is a connection then load missions
     func finishConnectionCheck(_ connected: Bool){
         self.tableView!.emptyDataSetSource = self
         self.tableView!.emptyDataSetDelegate = self
+        self.hasConnection = connected
         
-        if(!connected){
-            hasConnection = false
-            //Display a message if no connection
-            
+        if !connected {
             self.tableView!.reloadData()
             MRProgressOverlayView.dismissOverlay(for: self.view, animated: true)
-            //hasConnection = false
-        }else{
-            hasConnection = true
-            //API call to load missions
+        } else {
             CruClients.getServerClient().getData(DBCollection.SummerMission, insert: insertMission, completionHandler: reload)
-            
         }
-        
     }
 
     // Creates and inserts a SummerMission into this collection view from the given dictionary.
@@ -72,72 +65,24 @@ class SummerMissionsTableViewController: UITableViewController, SWRevealViewCont
         MRProgressOverlayView.dismissOverlay(for: self.view, animated: true)
     }
     
-    // MARK: - Empty Data Set Functions
-    func emptyDataSet(_ scrollView: UIScrollView!, didTap view: UIView!) {
-        if !hasConnection {
-            CruClients.getServerClient().checkConnection(self.finishConnectionCheck)
-        }
-    }
-    
-    func image(forEmptyDataSet scrollView: UIScrollView!) -> UIImage! {
-        if hasConnection {
-            return nil
-        }
-        else {
-            return UIImage(named: Config.noConnectionImageName)
-        }
-        
-    }
-    
-    func title(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
-        if hasConnection {
-            return nil
-        }
-        else {
-            let attributes = [ NSFontAttributeName: UIFont(name: Config.fontName, size: 18)!, NSForegroundColorAttributeName: UIColor.black]
-            return NSAttributedString(string: "Sorry, we're working on it! Check back later for more information about summer missions.", attributes: attributes)
-        }
-    }
-    
-    
     // MARK: - Table View Delegate Functions
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return missions.count
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let mission = missions[indexPath.row]
-        
-        if mission.imageLink == "" {
-            return 150.0
-        }
-        else {
-            return 275.0
-        }
+        return 275
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: self.reuseIdentifier, for: indexPath) as! SummerMissionsTableViewCell
+        
         let mission = missions[indexPath.row]
-        
-        cell.selectionStyle = UITableViewCellSelectionStyle.none
         cell.mission = mission
-        
-        cell.card.layer.shadowColor = UIColor.black.cgColor
-        cell.card.layer.shadowOffset = CGSize(width: 0, height: 1)
-        cell.card.layer.shadowOpacity = 0.25
-        cell.card.layer.shadowRadius = 2
 
         return cell
     }
     
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "missionDetails" {
             self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil)
@@ -151,23 +96,45 @@ class SummerMissionsTableViewController: UITableViewController, SWRevealViewCont
             missionDetailViewController.dateText = selectedMissionCell.missionDateLabel.text!
         }
     }
-    
-    //reveal controller function for disabling the current view
+}
+
+// MARK: - SWRevealViewControllerDelegate
+extension SummerMissionsTableViewController: SWRevealViewControllerDelegate {
     func revealController(_ revealController: SWRevealViewController!, willMoveTo position: FrontViewPosition) {
-        
         if position == FrontViewPosition.left {
             self.tableView.isScrollEnabled = true
             
             for view in self.tableView.subviews {
                 view.isUserInteractionEnabled = true
             }
-        }
-        else if position == FrontViewPosition.right {
+        } else if position == FrontViewPosition.right {
             self.tableView.isScrollEnabled = false
             
             for view in self.tableView.subviews {
                 view.isUserInteractionEnabled = false
             }
         }
+    }
+}
+
+// MARK: - Empty DataSet Delegate Functions
+extension SummerMissionsTableViewController: DZNEmptyDataSetDelegate, DZNEmptyDataSetSource {
+    func emptyDataSet(_ scrollView: UIScrollView!, didTap view: UIView!) {
+        guard !self.hasConnection else { return }
+
+        CruClients.getServerClient().checkConnection(self.finishConnectionCheck)
+    }
+    
+    func image(forEmptyDataSet scrollView: UIScrollView!) -> UIImage! {
+        guard !self.hasConnection else { return nil }
+        
+        return UIImage(named: Config.noConnectionImageName)
+    }
+    
+    func title(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
+        guard !self.hasConnection else { return nil }
+        
+        let attributes = [ NSFontAttributeName: UIFont(name: Config.fontName, size: 18)!, NSForegroundColorAttributeName: UIColor.black]
+        return NSAttributedString(string: "Sorry, we're working on it! Check back later for more information about summer missions.", attributes: attributes)
     }
 }
