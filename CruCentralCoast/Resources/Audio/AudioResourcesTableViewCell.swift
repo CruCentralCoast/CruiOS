5964ef1bb18915d86c39366177a11dd16b82533e
micantre <micantre@calpoly.edu>
2018-03-29 21:50:49 -0700

    updated some progress bar stuff on audio resources

--- CruCentralCoast/Resources/Audio/AudioResourcesTableViewCell.swift
+++ CruCentralCoast/Resources/Audio/AudioResourcesTableViewCell.swift
@@ -13,27 +13,35 @@ class AudioResourcesTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var currentTimeLabel: UILabel!
-    @IBOutlet weak var endTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    var lengthOfAudio: Int = 100
+    var currentTime: Float = 3 {
+        didSet {
+            self.currentTimeLabel.text = self.secondsToTimeString(numberOf: self.currentTime)
+        }
+    }
+    var duration: Float = 10
+    var audioIsPlaying: Bool = false
     
     override func awakeFromNib() {
-        self.progressBar.progress = 0
-        self.currentTimeLabel.text = self.secondsToTimeString(numberOf: 0)
-        self.endTimeLabel.text = self.secondsToTimeString(numberOf: self.lengthOfAudio)
+        self.progressBar.progress = self.currentTime/self.duration
+        self.currentTimeLabel.text = self.secondsToTimeString(numberOf: self.currentTime)
+        self.durationLabel.text = self.secondsToTimeString(numberOf: self.duration)
+        self.progressBar.setProgress(self.currentTime/self.duration, animated: false)
     }
     
     @IBAction func playPausePressed(_ sender: UIButton) {
         sender.tintColor = sender.tintColor == .cruBlue ? .red : .cruBlue
+        self.audioIsPlaying = !self.audioIsPlaying
     }
     
-    private func secondsToTimeString(numberOf seconds: Int) -> String {
-        let (h, m, s) = secondsToHoursMinutesSeconds(seconds: Double(seconds))
+    private func secondsToTimeString(numberOf seconds: Float) -> String {
+        let (h, m, s) = secondsToHoursMinutesSeconds(seconds: seconds)
         return String(format: "%02d:%02d:%02d", Int(h), Int(m), Int(s))
     }
     
-    private func secondsToHoursMinutesSeconds (seconds : Double) -> (Double, Double, Double) {
+    private func secondsToHoursMinutesSeconds (seconds : Float) -> (Float, Float, Float) {
         let (hr,  minf) = modf (seconds / 3600)
         let (min, secf) = modf (60 * minf)
         return (hr, min, 60 * secf)
