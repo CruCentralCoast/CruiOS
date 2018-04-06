a18d983a8ca012aacad69fc0e21fefc4ff4d85f0
tiedie211 <tchristiand@gmail.com>
2018-04-05 21:16:05 -0700

    Initial design of login screen

---
+++ CruCentralCoast/Login/LoginVC.swift
@@ -0,0 +1,38 @@
+//
+//  LoginVC.swift
+//  CruCentralCoast
+//
+//  Created by Tyler Dahl on 4/4/18.
+//  Copyright © 2018 Landon Gerrits. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signIn() {
+        
+    }
+    
+    @IBAction func signInWithFacebook() {
+    
+    }
+    
+    @IBAction func signInWithGoogle() {
+        
+    }
+    
+    @IBAction func forgotPassword() {
+        
+    }
+    
+    @IBAction func signUp() {
+        
+    }
+}
