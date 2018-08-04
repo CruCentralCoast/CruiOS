5bd6162d005f3720a5119e213e51bf623ccefd19
tiedie211 <tchristiand@gmail.com>
2018-08-04 12:18:02 -0700

    Removed extra test code from EventsVC

--- CruCentralCoast/Events/EventsVC.swift
+++ CruCentralCoast/Events/EventsVC.swift
@@ -12,13 +12,6 @@ import RealmSwift
 class EventsVC: UITableViewController {
     
     var dataArray: Results<Event>!
-    var people: Results<Person>!
-    var campuses: Results<Campus>!
-    var movements: Results<Movement>!
-    var communityGroups: Results<CommunityGroup>!
-    var ministryTeams: Results<MinistryTeam>!
-    var missions: Results<Mission>!
-    var resources: Results<Resource>!
     
     var statusBarIsHidden: Bool = false {
         didSet{
@@ -44,14 +37,7 @@ class EventsVC: UITableViewController {
         self.tableView.registerCell(EventsTableCell.self)
         
         DatabaseManager.instance.subscribeToDatabaseUpdates(self)
-//        self.people = DatabaseManager.instance.getPeople()
-//        self.campuses = DatabaseManager.instance.getCampuses()
-//        self.movements = DatabaseManager.instance.getMovements()
         self.dataArray = DatabaseManager.instance.getEvents()
-//        self.communityGroups = DatabaseManager.instance.getCommunityGroups()
-//        self.ministryTeams = DatabaseManager.instance.getMinistryTeams()
-//        self.missions = DatabaseManager.instance.getMissions()
-//        self.resources = DatabaseManager.instance.getResources()
 
         self.tableView.setContentOffset(tableView.contentOffset, animated: false)
         
@@ -82,39 +68,8 @@ class EventsVC: UITableViewController {
 }
 
 extension EventsVC: DatabaseListenerProtocol {
-    func updatedPeople() {
-        print("People were updated - refreshing UI")
-    }
-    func updatedCampuses() {
-        print("Campuses were updated - refreshing UI")
-    }
-    func updatedMovements() {
-        print("Movements were updated - refreshing UI")
-    }
     func updatedEvents() {
         print("Events were updated - refreshing UI")
-//        for event in self.dataArray {
-//            if let imageLink = event.imageLink {
-//                UIImage.downloadedFrom(link: imageLink, completion: { (image) in
-//                    event.image = image
-//                    DispatchQueue.main.async {
-//                        self.tableView.reloadData()
-//                    }
-//                })
-//            }
-//        }
         self.tableView.reloadData()
-    }
-    func updatedCommunityGroups() {
-        print("CommunityGroups were updated - refreshing UI")
-    }
-    func updatedMinistryTeams() {
-        print("MinistryTeams were updated - refreshing UI")
-    }
-    func updatedMissions() {
-        print("Missions were updated - refreshing UI")
-    }
-    func updatedResources() {
-        print("Resources were updated - refreshing UI")
     }
 }
