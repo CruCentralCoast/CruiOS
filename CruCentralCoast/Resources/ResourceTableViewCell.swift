591cb24ee9db05fb8c2a32c3ab0f4f6dd2655f10
Michael Cantrell <michaelcantrell@michaels-mbp.attlocal.net>
2018-08-11 17:09:27 -0700

    started making changes to resources

---
+++ CruCentralCoast/Resources/ResourceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ResourceTableViewCell.swift
+//  CruCentralCoast
+//
+//  Created by Michael Cantrell on 8/11/18.
+//  Copyright © 2018 Landon Gerrits. All rights reserved.
+//
+
+import UIKit
+
+class ResourceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
