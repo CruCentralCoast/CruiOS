2dc925226e0a06cd59799f0e48961d00e3c44307
Michael Cantrell <micantre@calpoly.edu>
2018-09-14 14:39:01 -0700

    adressing code review comments

--- CruCentralCoast/Login/ForgotPasswordVC.swift
+++ CruCentralCoast/Login/ForgotPasswordVC.swift
@@ -12,18 +12,46 @@ import FirebaseAuth
 
 class ForgotPasswordVC: UIViewController {
     @IBOutlet weak var emailTextField: CruTextField!
+    @IBOutlet weak var resetPasswordButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     var email: String? { didSet { self.emailTextField?.setText(self.email) } }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.emailTextField.setText(self.email)
+        
+        // Dismiss keyboard if view is tapped
+        let tapGesture = UITapGestureRecognizer(target: self.view, action: #selector(self.view.endEditing))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        // Listen for keyboard events
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
     }
     
-    @IBAction func sendEmailButtonPressed(_ sender: Any) {
-        guard let email = self.emailTextField.text, !email.isEmpty else {
-            self.presentAlert(title: "Email Field Empty", message: "Type the email associated with the account that you want to reset the password of")
-            return
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            let buttonPosition = self.loginButton.frame.origin.y + self.loginButton.frame.height
+            let keyboardPosition = self.view.frame.height - keyboardSize.height
+            let buffer: CGFloat = 10
+            if buttonPosition + buffer >= keyboardPosition {
+                self.view.frame.origin.y -= buttonPosition + buffer - keyboardPosition
+            }
         }
+    }
+    
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        if let _ = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0 {
+                self.view.frame.origin.y = 0
+            }
+        }
+    }
+    
+    @IBAction func resetPasswordButtonPressed() {
+        self.emailTextField.validateHasText()
+        guard let email = self.emailTextField.text, !email.isEmpty else { return }
         
         Auth.auth().sendPasswordReset(withEmail: email) { error in
             if let error = error {
