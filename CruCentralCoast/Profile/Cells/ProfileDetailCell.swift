cc5df4c40e5e4bb1ad7f164210acad2173a69b60
tiedie211 <tchristiand@gmail.com>
2018-09-24 22:05:20 -0700

    Removed unused functions from ProfileDetailCell

--- CruCentralCoast/Profile/Cells/ProfileDetailCell.swift
+++ CruCentralCoast/Profile/Cells/ProfileDetailCell.swift
@@ -12,17 +12,6 @@ class ProfileDetailCell: UITableViewCell {
 
     @IBOutlet weak var label: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
     func configure(with text: String) {
         self.label.text = text
     }
